d2059288dfd3a260f04758e021ebf696c2b693f2
liu kelin <314566990@qq.com>
2017-07-28 18:20:31 +0800

    api

--- api/search.php
+++ api/search.php
@@ -1,3 +1,21 @@
 <?php
-require __DIR__.'/elastic/vendor/autoload.php'; 
-$client = new Elasticsearch\Client('127.0.0.1',9200);
+require_once(__DIR__.'/elastic/vendor/autoload.php'); 
+// $client = new Elasticsearch\Client('127.0.0.1:9200');
+$hosts = array('192.168.1.10');
+$client = Elasticsearch\ClientBuilder::create()->setHosts($hosts)->build();
+// $client = $client->getElasticClient();
+$params = array(
+    'index' => 'website',
+    'type' => 'blog',
+    'body' => array(
+        'query' => array(
+            'match' => array(
+                '_id' => 1,
+           )
+        )
+    )
+);
+$rtn = $client->search($params);
+echo '<pre>';
+print_r($rtn);
+echo '</pre>';
