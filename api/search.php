9f95ea625d1979f1f1dca2eb250eb06d40af88ac
liu kelin <314566990@qq.com>
2017-07-28 18:28:18 +0800

    api

--- api/search.php
+++ api/search.php
@@ -1,9 +1,6 @@
 <?php
 require_once(__DIR__.'/elastic/vendor/autoload.php'); 
-// $client = new Elasticsearch\Client('127.0.0.1:9200');
-$hosts = array('192.168.1.10:9200');
-$client = Elasticsearch\ClientBuilder::create()->setHosts($hosts)->build();
-// $client = $client->getElasticClient();
+
 $params = array(
     'index' => 'test-ik',
     'type' => 'test-ik-smart',
@@ -15,6 +12,12 @@ $params = array(
         )
     )
 );
+// $client = new Elasticsearch\Client('127.0.0.1:9200');
+// $docs = $client->search($params);
+$hosts = array('http://127.0.0.1:9200');
+$client = Elasticsearch\ClientBuilder::create()->setHosts($hosts)->build();
+// $client = $client->getElasticClient();
+
 $rtn = $client->search($params);
 echo '<pre>';
 print_r($rtn);
