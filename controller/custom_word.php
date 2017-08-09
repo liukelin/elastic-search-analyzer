c690ca8ec49167aeabfde224a40a5202830cd923
liu kelin <314566990@qq.com>
2017-08-09 12:03:07 +0800

    demo

--- controller/custom_word.php
+++ controller/custom_word.php
@@ -51,10 +51,18 @@ switch ($action) {
     $s = <<<'EOF'
         {$words}
 EOF;
+
     // 为了避免对es造成不必要的压力，最好做成更新了词库 再改变头部属性 
-    $ETag = time(); 
+    $ETag = '"' . time() . '"';
     header('Last-Modified: '.gmdate('D, d M Y H:i:s', time()).' GMT', true, 200);
     // header('ETag: "5816f348-23"');
-    header('ETag: "'.$ETag.'"');
+    header("ETag: $ETag");
     echo $s;
 }
+
+
+
+
+
+
+
