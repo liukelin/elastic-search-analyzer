8937abb68b7bf185cbc7a3fbad551cbd2dc3fd7e
liu kelin <314566990@qq.com>
2017-08-09 15:00:09 +0800

    demo

--- controller/custom_word.php
+++ controller/custom_word.php
@@ -14,50 +14,73 @@ header("Content-type: text/html; charset=utf-8");
  * http://xxx/api/custom_word.php?action=stop
  * 
  */
-
 $action = isset($_GET['action'])?$_GET['action']:null;
 $dir = __DIR__.'/../words/';
 $words = '';
-$file = array();
-// $action = 'hot';
+$files = array();
+$action = 'hot';
 
 switch ($action) {
 
     case 'hot': // 扩展词
         
         $dir = $dir.'hot/';
-        $file = scandir($dir);
+        $files = scandir($dir);
 
         break;  
     case 'stop': // 扩展停止词
 
         $dir = $dir.'stop/';
-        $file = scandir($dir);
+        $files = scandir($dir);
 
         break;
     default:
         break;
-
-    foreach ($file as $key => $v) {
-        if (pathinfo($dir.$v)['extension']=='dic') {
-            $words .= @include($dir.$v);
-            $words .= "\n"; // 添加一个换行符
-        }
-    }
-    $words = trim($words);
-
-    $s = <<<'EOF'
-        {$words}
-EOF;
-
-    // 为了避免对es造成不必要的压力，最好做成更新了词库 再改变头部属性 
-    $ETag = '"' . time() . '"';
-    // header('Last-Modified: '.gmdate('D, d M Y H:i:s', time()).' GMT', true, 200);
-    header('Last-Modified: '.gmdate('D, d M Y H:i:s', time() ).' GMT');
-    // header('ETag: "5816f348-23"');
-    header("ETag: $ETag");
-    echo $s;
 }
+/**
+foreach ($files as $key => $v) {
+    if (pathinfo($dir.$v)['extension']=='dic') {
+        $words .= @include($dir.$v);
+        $words .= "\n"; // 添加一个换行符
+    }
+}
+$words = trim($words);
+
+$s = <<<'EOF'
+    {$words}
+EOF;
+**/
+
+// 行读取
+foreach ($files as $key => $v) {
+    $f = $dir.$v;
+    if (pathinfo($f)['extension']=='dic') {
+        if (is_file($f)) {
+
+            $file = @fopen($f, "r");
+            while(!@feof($file)){
+                $work = @fgets($file);
+                $work = trim($work);
+                if ($work) {
+                    $words .= $work."\n";
+                }
+            }
+            @fclose($file);
+        }
+
+    }
+}
+$words = trim($words);
+
+// 为了避免对es造成不必要的压力，最好做成更新了词库 再改变头部属性 
+$ETag = '"' . time() . '"';
+header('Last-Modified: '.gmdate('D, d M Y H:i:s', time()).' GMT', true, 200);
+// header('Last-Modified: '.gmdate('D, d M Y H:i:s', time() ).' GMT');
+// header('ETag: "5816f348-23"');
+header("ETag: $ETag");
+exit($words);
+?>
+
 
 
 
